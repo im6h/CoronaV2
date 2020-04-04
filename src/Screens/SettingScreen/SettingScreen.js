e730ae122ea2fc15eb5889c5dfd53607d8a048f0
tjjone98 <tzero1898@gmail.com>
2020-04-04 20:17:28 +0700

    feat[build]: setup to build apk

---
+++ src/Screens/SettingScreen/SettingScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+class SettingScreen extends React.Componet {
+  render() {
+    return (
+      <View>
+        <Text>This is setting screen</Text>
+      </View>
+    );
+  }
+}
+const styles = StyleSheet.create({});
+export default SettingScreent;
